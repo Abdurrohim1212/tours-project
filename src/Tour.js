1d3d6ae781560dffe43d1afb87824061d109c263
Abdurrohim_ethan <abdurrohim48@gmail.com>
2021-03-27 11:15:17 +0700

    changing currency from dollar us to rupiah

--- src/Tour.js
+++ src/Tour.js
@@ -7,7 +7,9 @@ const Tour = ({ id, image, info, price, name }) => {
       <footer>
         <div className="tour-info">
           <h4>{name}</h4>
-          <h4 className="tour-price">${price}</h4>
+          <h4 className="tour-price">
+            IDR {Math.ceil(parseInt(price)) * 14415}K
+          </h4>
         </div>
       </footer>
     </article>
